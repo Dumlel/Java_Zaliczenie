0e9fa5b34d6fb4c005684e95438ecdd6d7626270
Dumlel <djakacka@gmail.com>
2021-05-22 12:44:03 +0200

    3. Każdy pojazd ma swój numer rejestracyjny, numer vin, kolor, cenę, spalanie, stan zbiornika paliwa oraz licznik przejechanych kilometrów.

--- src/pl/gda/wsb/Vehicle.java
+++ src/pl/gda/wsb/Vehicle.java
@@ -1,12 +1,26 @@
 package pl.gda.wsb;
 
 public abstract class Vehicle {
-public Vehicle(String registration, String vin, String color, float price, float fuelComsumption, float tankLevel, float Odometer){
 
-
-
-
-
-
+    public Vehicle(String registration, String vin, String color, float price, float fuelComsumption, float tankLevel, float Odometer){
+        this.registration = registration;
+        this.vin = vin;
+        this.color = color;
+        this.price = price;
+        this.fuelComsumption = fuelComsumption;
+        this.tankLevel = tankLevel;
+        this.Odometer = Odometer;
 }
+    private String registration;
+    private String vin;
+    private String color;
+    private float price;
+    private float fuelComsumption;
+    private float tankLevel;
+    private float Odometer;
+
+
+
+
+
 }
