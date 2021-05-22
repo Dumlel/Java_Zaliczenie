fdba596bea3c5625b300ebf1da1386a7f1fa77b1
Dumlel <djakacka@gmail.com>
2021-05-22 12:38:14 +0200

    1. Stworzyć nowy projekt Java. 2. Stworzyć klasę abstrakcyjną Vehicle. 4. Stworzenie klas dla samochodów osobowych, ciężarowych i motocykli (klasy car, truck, motorcycle dziedzicące po Vehicle).

--- src/pl/gda/wsb/Vehicle.java
+++ src/pl/gda/wsb/Vehicle.java
@@ -1,5 +1,12 @@
 package pl.gda.wsb;
 
 public abstract class Vehicle {
+public Vehicle(String registration, String vin, String color, float price, float fuelComsumption, float tankLevel, float Odometer){
 
+
+
+
+
+
+}
 }
